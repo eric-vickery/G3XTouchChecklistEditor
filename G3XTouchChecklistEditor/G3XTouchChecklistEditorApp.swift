35969d0fde3f748a5c8fd1ad6d0fc3ff0d329fb1
Eric Vickery <evickery@repay.com>
2023-12-29 18:53:03 -0700

    Initial Commit

---
+++ G3XTouchChecklistEditor/G3XTouchChecklistEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  G3XTouchChecklistEditorApp.swift
+//  G3XTouchChecklistEditor
+//
+//  Created by Eric Vickery on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct G3XTouchChecklistEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
